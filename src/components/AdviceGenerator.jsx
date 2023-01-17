568f74839b564ac6f5f7ae07c5bc08a49cc14c8f
Alberto Millán <90913818+AlbertoMillanC@users.noreply.github.com>
2023-01-16 20:46:41 -0500

    format

--- src/components/AdviceGenerator.jsx
+++ src/components/AdviceGenerator.jsx
@@ -1,32 +1,33 @@
-import { useState } from 'react'
-import { useEffect } from 'react'
-
+import { useState } from "react";
+import { useEffect } from "react";
 
 function getAdvice() {
-  const url = 'https://jsonplaceholder.typicode.com/todos'  
-  const [todos,setTodos] = useState()
+  const url = "https://jsonplaceholder.typicode.com/todos";
+  const [todos, setTodos] = useState();
   const fetchApi = async () => {
-    const response = await fetch(url)
-    const responseJSON = await response.json() 
-    setTodos(responseJSON)
-
-  }
-  useEffect (() => {
-    fetchApi()
-  },[])
+    const response = await fetch(url);
+    const responseJSON = await response.json();
+    setTodos(responseJSON);
+  };
+  useEffect(() => {
+    fetchApi();
+  }, []);
   return (
     <div>
       <h1 className="bg-slate-100 text-">GENERADOR </h1>
       <ul>
-        
-      
-      {!todos ? 'cargando...' :
-      todos.map((todo,index)=>{
-        return <li key={index}>{ todo.id}, {todo.title}, {todo.userId},{todo.body} </li>
-      }) }
+        {!todos
+          ? "cargando..."
+          : todos.map((todo, index) => {
+              return (
+                <li key={index}>
+                  {todo.id}, {todo.title}, {todo.userId},{todo.body}{" "}
+                </li>
+              );
+            })}
       </ul>
     </div>
-  )
+  );
 }
 
-export default getAdvice
+export default getAdvice;
